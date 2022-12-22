b06473e1402cdca19923cf09fd82c0cf27bfdf89
TatShen <shenets-97@mail.ru>
2022-12-22 22:23:27 +0300

    delete path

--- src/components/atoms/Logo/Logo.js
+++ src/components/atoms/Logo/Logo.js
@@ -8,7 +8,7 @@ export class Logo extends Component{
     render(){
         return `
         <div class="logo-border">
-            <img class="logo" src="/src/assets/svg/hot-air-balloon-svgrepo-com.svg">
+            <img class="logo" src="src/assets/svg/hot-air-balloon-svgrepo-com.svg">
         </div>
         `
     }
